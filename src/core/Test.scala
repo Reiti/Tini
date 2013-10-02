a9a5c0cd8263219bd2695f119774fb8a60b70887
Michael Reitgruber <reitgruber.michael@gmail.com>
2013-10-02 13:37:25 +0200

    Initial commit

---
+++ src/core/Test.scala
@@ -0,0 +1,14 @@
+package core
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: NotReiti
+ * Date: 02.10.13
+ * Time: 13:35
+ * To change this template use File | Settings | File Templates.
+ */
+object Test {
+  def main(args: Array[String]) {
+    println("test")
+  }
+}
