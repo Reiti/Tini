6c917f4c989d6f4f61938964e6721807ad690872
Michael Reitgruber <reitgruber.michael@gmail.com>
2013-10-03 17:51:16 +0200

    Added List of Thread Handles

--- src/server/core/TiniServer.scala
+++ src/server/core/TiniServer.scala
@@ -1,6 +1,9 @@
 package server.core
 
 import java.net.ServerSocket
+import java.util
+import scala.collection.mutable._
+import scala.collection.mutable
 
 /**
  * Created with IntelliJ IDEA.
@@ -10,10 +13,14 @@ import java.net.ServerSocket
  * The one listener of Tini to bind them all.
  */
 class TiniServer(port: Integer) extends Thread("TiniServer") {
+  var clientThreadHandles:ArrayBuffer[ServerThread] = new ArrayBuffer[ServerThread] with mutable.SynchronizedBuffer[ServerThread]
   override def run() = {
     val listener = new ServerSocket(port)
     println("TiniServer started listening on port " + port)
-    while (true)
-      new ServerThread(listener accept) start()
+    while (true) {
+      val thread = new ServerThread(listener accept)
+      clientThreadHandles += thread
+      thread start()
+    }
   }
 }
