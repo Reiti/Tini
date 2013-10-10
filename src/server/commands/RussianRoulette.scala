d9b6a1a0f51916dc254ff8ea1c8b9c5e40176f08
Benjamin Potzmann <benjamin.potzmann@gmail.com>
2013-10-10 08:51:04 +0200

    Implements /russianroulette. Forgot that file in the last commit...

---
+++ src/server/commands/RussianRoulette.scala
@@ -0,0 +1,24 @@
+package server.commands
+
+import server.core.ServerThread
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Giymo11
+ * Date: 10.10.13
+ * Time: 08:18
+ * The class representing the command "/russianroulette".
+ */
+class RussianRoulette(params:Array[String]) extends Command(params) {
+  def ACTION: String = "/russianroulette"
+
+  def execute(fred: ServerThread) {
+    val random:Integer = (Math.random() * 6.0).toInt
+    println("Random: "+ random)
+    if(random == 0){
+      fred.breadCastToAll("/say Server " + fred.username + " played russian roulette and shot himself.")
+      new Disconnect(null).execute(fred)
+    } else
+      fred.breadCastToAll("/say Server " + fred.username + " played russian roulette and survived. (" + random + ")")
+  }
+}
